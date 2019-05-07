8b9da446caf5af8366522d3dc6bc0c21a19c550a
ZhangC <82036136@qq.com>
2019-05-07 15:47:24 +0800

    111

---
+++ test/Hello.java
@@ -0,0 +1,12 @@
+package com.itheima.test;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("万水千山总是情");
+        System.out.println("万水千山总是情");
+        System.out.println("万水千山总是情");
+        System.out.println("万水千山总是情");
+        System.out.println("万水千山总是情");
+        System.out.println("万水千山总是情");
+    }
+}
